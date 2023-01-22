d9fb02c732b42d5fb8b1bee44542f06030ee752c
Michał Żelazko <242149@edu.p.lodz.pl>
2023-01-22 18:23:27 +0100

    sends an empty record

--- inProgressSSPLPage/src/components/Newsletter.tsx
+++ inProgressSSPLPage/src/components/Newsletter.tsx
@@ -8,6 +8,30 @@ export const Newsletter = () => {
     setEnteredEmail(event.target.value);
   };
 
+  const submitHandler = (event: any) => {
+    const formElement = document.querySelector("form");
+    event.preventDefault();
+    const data = new URLSearchParams();
+    data.append("email", enteredEmail);
+
+    fetch(
+      "https://script.google.com/macros/s/AKfycbxaoYW5UF76L0j_OUoym6nOpwq9j94hpxI9MVl5EZvAjWH0YxNnstlyga2zy3jb4fIe/exec",
+      {
+        method: "POST",
+        body: data,
+        headers: {
+          "Content-Type": "text/plain;charset=UTF-8",
+        },
+      }
+    )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <section className="newsletter-area">
       <div className="container">
@@ -15,19 +39,20 @@ export const Newsletter = () => {
           <div className="col-lg-8">
             <h5 className="has-line">Strona w przygotowaniu</h5>
             <h1>Powiadomimy Cię, kiedy będzie gotowa</h1>
-            <form action="#">
+            <form
+              onSubmit={(e) => {
+                submitHandler(e);
+              }}
+            >
               <div className="form-group">
                 <input
                   type="email"
+                  name="email"
                   placeholder="Podaj adres e-mail"
                   pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
                   onChange={emailChangeHandler}
                 />
-                <button
-                  type="submit"
-                  onClick={() => console.log(enteredEmail)}
-                  // TODO onSubmit={execute google script}
-                >
+                <button type="submit" onClick={() => console.log(enteredEmail)}>
                   Otrzymaj powiadomienie
                 </button>
               </div>
