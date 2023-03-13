15a8e7370ed399bd91144ea69b59ce181a1995fc
Patryk Makowski <patryk.makowski23@gmail.com>
2023-03-13 22:46:21 +0100

    Fix plausible

--- README.md
+++ README.md
@@ -2,13 +2,6 @@
 
 Page that is to be a placeholder for pages that are currently under maintenance or not deployment-ready. It allows the user to enter and send their email to our Google Workspace database, so that we notify when the page they were looking for is ready.
 
-To add analytics add app on [plausible](https://plausible.samorzad.p.lodz.pl) and add url in format (example.samorzad.p.lodz.pl) as DOMAIN in `.env`
-
-```
-# .env
-DOMAIN=example.samorzad.p.lodz.pl
-```
-
 ![](screenshot.png)
 
 ## License
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import "./vendor/bootstrap.min.css";
 import "./main.css";
-import { Helmet } from "react-helmet";
 import { Navbar } from "./components/Navbar";
 import { Newsletter } from "./components/Newsletter";
 import { MobileImage } from "./components/MobileImage";
+import { Helmet } from "react-helmet";
 
 function App() {
+  const location = window.location.hostname;
+
   return (
     <section className="main">
       <Helmet>
         <script
           defer
-          data-domain={import.meta.env.DOMAIN}
+          data-domain={location}
           src="https://plausible.samorzad.p.lodz.pl/js/script.js"
         ></script>
       </Helmet>
