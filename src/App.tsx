bc63ff861e786739955af41357caf62bdd0f6264
Patryk Makowski <patryk.makowski23@gmail.com>
2023-03-13 22:38:12 +0100

    Add plausible analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./vendor/bootstrap.min.css";
 import "./main.css";
+import { Helmet } from "react-helmet";
 import { Navbar } from "./components/Navbar";
 import { Newsletter } from "./components/Newsletter";
 import { MobileImage } from "./components/MobileImage";
@@ -7,6 +8,13 @@ import { MobileImage } from "./components/MobileImage";
 function App() {
   return (
     <section className="main">
+      <Helmet>
+        <script
+          defer
+          data-domain={import.meta.env.DOMAIN}
+          src="https://plausible.samorzad.p.lodz.pl/js/script.js"
+        ></script>
+      </Helmet>
       <Navbar />
       <Newsletter />
       <MobileImage />
